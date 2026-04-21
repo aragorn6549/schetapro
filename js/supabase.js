d3c0d35b32ee64beda750f81e320785d789e85ef
aragorn6549 <aragorn6549narlas@gmail.com>
2026-04-21 21:18:17 +0300

    Update supabase.js

--- js/supabase.js
+++ js/supabase.js
@@ -1,7 +1,7 @@
 // ⚠️ ВНИМАНИЕ: На следующем шаге мы создадим проект в Supabase.
 // Тогда замени строки ниже на свои реальные данные из настроек Supabase.
-const SUPABASE_URL = 'ВСТАВЬ_СВОЙ_SUPABASE_URL';
-const SUPABASE_ANON_KEY = 'ВСТАВЬ_СВОЙ_SUPABASE_ANON_KEY';
+const SUPABASE_URL = 'https://exqaoxsuqhjgkdzmzhuq.supabase.co';
+const SUPABASE_ANON_KEY = 'sb_publishable_cptCfdzxFGF0I9j9FXLMfg_UJSzfkPS';
 
 // Инициализация клиента (работает через CDN, подключенный в HTML)
 const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
